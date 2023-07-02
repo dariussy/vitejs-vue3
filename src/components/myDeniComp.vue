22b6f5f5eea5a91851fca113425c45d9e30a3d19
Deni GJUZI <dgjuzi@MacBook-Air-de-Deni.local>
2023-07-02 13:32:49 +0200

    first commit

---
+++ src/components/myDeniComp.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>ici{{ name }}</h2>
+    <h2>iciddd-{{ name2 }}</h2>
+</template>
+<script setup lang="ts">
+    //defineProps({name:String})
+    interface props {
+        name2:string
+        name:string
+    }
+    const props = withDefaults(defineProps<props>(),{ 
+        name2 : 'defaultName2',name : 'defaultName2'}        )
+</script>
