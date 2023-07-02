08a7b3beabef5debbb125a8250647e9db0cbd75e
Deni GJUZI <dgjuzi@MacBook-Air-de-Deni.local>
2023-07-02 13:32:49 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from "./router/router.ts";
-import { createPinia } from 'pinia'
 
-createApp(App)
-    .use(createPinia())
-    .use(router)
-    .mount('#app')
+createApp(App).mount('#app')
