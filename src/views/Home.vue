080c8fb7ce6c069536ba9b4746388ce8f8b1a765
dgjuzi <d.dariussy@gmail.com>
2024-02-23 23:27:42 +0100

    pinia avec authJWT

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex h-screen font-roboto">
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <my-header/>
+      <main class="flex-1 overflow-x-hidden overflow-y-auto bg-slate-50">
+        <div class="container mx-auto px-6 py-8">
+
+
+          <article class="text-wrap ...">
+            <h3>Token information's in store: </h3>
+            <div v-show="authStore.isTokenEmpty">
+              <p>no-token-please-login</p>
+            </div>
+            <div>
+              <p>{{authStore?.getTokenData?.access_token}}</p>
+            </div>
+          </article>
+
+
+        </div>
+      </main>
+    </div>
+  </div>
+
+</template>
+<script setup lang="ts">
+import MyHeader from "@/components/MyHeader.vue";
+import {useAuthStore} from "@/services/auth.service.ts";
+const authStore = useAuthStore();
+
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import module from "vite";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
