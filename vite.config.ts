a6755613951721705e9cd57e37b335cadff4bcd1
dgjuzi <d.dariussy@gmail.com>
2024-02-23 23:27:42 +0100

    pinia avec authJWT

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base : "/vitejs-vue3/"
+  resolve : {
+    alias:{
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
