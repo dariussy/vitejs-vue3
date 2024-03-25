818730481153403e61338fda9485f60db6a53557
dgjuzi <d.dariussy@gmail.com>
2024-03-25 14:47:21 +0100

    merge + env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url';
+import dotenv from 'dotenv';
+dotenv.config()
 
 export default defineConfig({
     plugins: [vue()],
-    base : "/vitejs-vue3/",
+    base : process.env.BASE_URL,
     resolve : {
         alias:{
         '@': fileURLToPath(new URL('./src', import.meta.url))
